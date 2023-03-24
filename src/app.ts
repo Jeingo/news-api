2d73b136bb56911b9e734410bf20bfb52325ebfb
Renat <jeingo20@gmail.com>
2023-03-25 01:30:33 +0300

    Add: cors *

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,15 @@ import cookieParser from 'cookie-parser'
 import { authRouter } from './feature/auth/api/router/auth.router'
 import { newsRouter } from './feature/news/api/router/news.router'
 import { testRouter } from './feature/testing/api/router/testing.router'
+import cors from 'cors'
 
 export const app = express()
 
+app.use(
+    cors({
+        origin: '*'
+    })
+)
 app.use(express.json())
 app.use(cookieParser())
 
