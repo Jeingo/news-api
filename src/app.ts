b2fd042498900fd095f7ec8c84db84e15788e897
Renat <jeingo20@gmail.com>
2023-03-24 15:58:53 +0300

    Add: base routers

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import { authRouter } from './feature/auth/router/auth.router'
+import { newsRouter } from './feature/news/router/news.router'
+import { testRouter } from './feature/testing/router/testing.router'
 
 export const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
+app.set('trust proxy', true) // for get correct ip address
+
+app.use('api/auth', authRouter)
+app.use('api/news', newsRouter)
+app.use('api/testing/delete-all-data', testRouter)
