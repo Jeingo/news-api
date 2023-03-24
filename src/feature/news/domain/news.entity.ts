a6bcaa7484266ee3c85f1c00eb41bc0b8d0c2592
Renat <jeingo20@gmail.com>
2023-03-24 21:31:20 +0300

    Add: News entity

---
+++ src/feature/news/domain/news.entity.ts
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+import { News, NewsDocument, NewsModelFullType } from './news.entity.type'
+
+export const NewsSchema = new mongoose.Schema<News>({
+    title: { type: String, required: true, maxlength: 100, minlength: 3 },
+    description: { type: String, maxlength: 200, required: true },
+    content: { type: String, maxlength: 1000, required: true },
+    createdAt: { type: String, required: true },
+    published: { type: Boolean, required: true },
+    static_img: { type: String }
+})
+
+NewsSchema.statics.make = function (title: string, description: string, content: string) {
+    const newDate = new Date()
+    return new NewsModel({
+        title: title,
+        description: description,
+        content: content,
+        createdAt: newDate.toISOString(),
+        published: false,
+        static_img: '/img'
+    })
+}
+
+NewsSchema.methods.update = function (title: string, description: string, content: string): boolean {
+    this.title = title
+    this.description = description
+    this.content = content
+    return true
+}
+
+NewsSchema.methods.publish = function (status: boolean): boolean {
+    this.published = status
+    return true
+}
+
+export const NewsModel = mongoose.model<NewsDocument, NewsModelFullType>('news', NewsSchema)
---
+++ src/feature/news/domain/news.entity.type.ts
@@ -0,0 +1,22 @@
+import { Model, Types, HydratedDocument } from 'mongoose'
+
+export type News = {
+    title: string
+    description: string
+    content: string
+    createdAt: string
+    published: boolean
+    static_img: string
+}
+export type NewsDocument = HydratedDocument<News> & NewsMethods
+
+type NewsStatics = {
+    make: (title: string, description: string, content: string) => NewsDocument
+}
+
+export type NewsMethods = {
+    update: (title: string, description: string, content: string) => boolean
+    publish: (status: boolean) => boolean
+}
+
+export type NewsModelFullType = Model<NewsDocument> & NewsStatics & { _id: Types.ObjectId }
